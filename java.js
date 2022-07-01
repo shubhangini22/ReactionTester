b76c3304d7fdf03723691206e0853e3ca4dc0656
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-07-01 09:46:30 +0530

    updated

--- java.js
+++ java.js
@@ -1,10 +1,11 @@
 var start = new Date().getTime();
-            
+            //Date().getTime function here gets the current time
             function getRandomColor() {
-                
+                //function to get random color for the shapes
                 var letters = '0123456789ABCDEF'.split('');
-    
+               //.split() functions splits the array here into array
                 var color = '#';
+                //html colors strats with #
     
                 for (var i = 0; i < 6; i++ ) {
         
@@ -14,18 +15,20 @@ var start = new Date().getTime();
     
                 return color;
 
+
             }
             
             function makeShapeAppear() {
                 
                 var top = Math.random() * 400;
-                
+                //randomizing positions
                 var left = Math.random() * 400;
                 
                 var width = (Math.random() * 200) + 100;
-                
+                //width is use for randomizing the size
                 if (Math.random() > 0.5) {
-                    
+                    //half of a time it wil be circle 
+                    //circle
                     document.getElementById("shape").style.borderRadius = "50%";
                     
                 } else {
@@ -47,24 +50,27 @@ var start = new Date().getTime();
                 document.getElementById("shape").style.display = "block";
                 
                 start = new Date().getTime();
+                //updating the start variable
 
             }
             
             function appearAfterDelay() {
                 
                 setTimeout(makeShapeAppear, Math.random() * 2000);
-                
+                //setTimeout function is used here to make shape appear after randomized 0-2000 here interval for testing reaction
+               
             }
             
             appearAfterDelay();
             
             document.getElementById("shape").onclick = function() {
-                
+                //this onclick function is for making the shape disappear
                 document.getElementById("shape").style.display = "none";
-                
+                //none here makes it disappear
                 var end = new Date().getTime();
                 
                 var timeTaken = (end - start) / 1000;
+                //time taken since the start of the page loading and end of the reaction of the user
                 
                 document.getElementById("timeTaken").innerHTML = timeTaken + "s";
                 
